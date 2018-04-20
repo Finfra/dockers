dc9d9f8039ba73aa82b05d0be2f53d2094d5bffa
NamJungGu <nowage@gmail.com>
2018-04-20 17:21:25 +0900

    updated - Tensor flow

--- tensorflow/install.sh
+++ tensorflow/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-sudo apt-get update
+apt-get update
 apt-get -y install python3-pip
+pip3 install --upgrade pip3
 pip3 install tensorflow
 pip3 install ipykernel
-
+python3 -m ipykernel install --user --name=Python3
+pip3 install jupyter
+rm -rf /usr/bin/python
+ln -s /usr/bin/python3 /usr/bin/python
