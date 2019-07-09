6a34344da88581e300d62c6d1e64958448550cfc
NamJungGu <nowage@gmail.com>
2019-07-09 15:32:37 +0900

    modified:   ubuntu_user/start.sh

--- ubuntu_user/start.sh
+++ ubuntu_user/start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
 mkdir /home/$USER
 useradd -d /home/$USER $USER 
 chown $USER:$USER /home/$USER
 echo -e "$PASSWD\n$PASSWD" | passwd $USER
 echo  $USER password: $PASSWD
-
-
-
-
 /bin/bash
