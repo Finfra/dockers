b0d77a8fdca36da9db3bcbdea9696842c1f88bbd
Steve J. South(NamJungGu) <nowage@gmail.com>
2025-04-28 23:29:02 +0900

    .

--- wordpress_adv/wp-config.php
+++ wordpress_adv/wp-config.php
@@ -46,8 +46,8 @@ if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
 }
 
 // ** 사이트 URL 설정 (설치 완료 후에는 DB에 저장됨) ** //
-define('WP_SITEURL', 'http://localhost:8080');
-define('WP_HOME', 'http://localhost:8080');
+define('WP_SITEURL', getenv_docker('WORDPRESS_URL', 'http://localhost:8080'));
+define('WP_HOME', getenv_docker('WORDPRESS_URL', 'http://localhost:8080')); 
 
 // ** WordPress 디렉토리 경로 ** //
 if (!defined('ABSPATH')) {
@@ -57,18 +57,15 @@ if (!defined('ABSPATH')) {
 // ** WordPress 설정 파일 로드 ** //
 require_once ABSPATH . 'wp-settings.php';
 
-/** SSL 설정 */
-define('WP_FORCE_SSL_ADMIN', true);
+define('WP_FORCE_SSL_ADMIN', false);
 define('FORCE_SSL_ADMIN', true);
-
-/** 환경 타입 설정 - 개발 환경으로 변경 */
-define('WP_ENVIRONMENT_TYPE', 'development');
-
-/** 응용 프로그램 비밀번호 강제 활성화 */
+define('WP_ENVIRONMENT_TYPE', 'staging');
+define( 'SCRIPT_DEBUG', true );
 define('WP_APPLICATION_PASSWORDS_ENABLED', true);
 
-/** 디버그 스크립트 설정 */
-define('SCRIPT_DEBUG', true);
+
+
+
 
 // ** WordPress 설정 파일 로드 ** //
 require_once ABSPATH . 'wp-settings.php';
